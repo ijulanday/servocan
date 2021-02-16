ab3ae812fb3ff7c33a2d43e33d05cdf27331aae2
ian ulanday <ulanday.ian@gmail.com>
2021-02-16 10:02:25 -0700

    changed functions to include CAN bus ID specification, added calls to changing servo CAN bus IDs

--- servocan.cc
+++ servocan.cc
@@ -12,10 +12,11 @@ void printRaw(CAN_message_t servo_message) {
 }
 
 // generic for write messages
-void genericWriteMessage(uint8_t address, uint8_t servoId, uint16_t data, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can) {
+void genericWriteMessage(uint32_t canId, bool extended, uint8_t address, uint8_t servoId, uint16_t data, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can) {
     uint8_t lo = data & 0x00FF;
     uint8_t hi = data >> 8;
-    servo_message.id = 0x000;         // specify hitec servo
+    servo_message.id = canId;         // specify hitec servo CAN ID (0x000 if default)
+    servo_message.flags.extended = extended;
     servo_message.buf[0] = 0x96;      // write message header
     servo_message.buf[1] = servoId;   // servo id
     servo_message.buf[2] = address;      // address
@@ -28,8 +29,9 @@ void genericWriteMessage(uint8_t address, uint8_t servoId, uint16_t data, FlexCA
 }
 
 // generic for read messages
-void genericReadMessage(uint8_t address, uint8_t servoId, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can) {
-    servo_message.id = 0x000;
+void genericReadMessage(uint32_t canId, bool extended, uint8_t address, uint8_t servoId, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can) {
+    servo_message.id = canId;
+    servo_message.flags.extended = extended;
     servo_message.buf[0] = 0x96;
     servo_message.buf[1] = servoId;
     servo_message.buf[2] = address;
@@ -45,69 +47,69 @@ uint16_t genericDecoder(CAN_message_t msg) {
 }
 
 // moves servo to an angle from -180 to 180 degrees
-void REG_POSITION_NEW(uint8_t servoId, double usrAngle, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can) {
-      genericWriteMessage(0x1E, servoId, uint16_t(usrAngle*ANGLE_CONVERSION+8192), can);
+void REG_POSITION_NEW(uint32_t canId, bool extended, uint8_t servoId, double usrAngle, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can) {
+      genericWriteMessage(canId, extended, 0x1E, servoId, uint16_t(usrAngle*ANGLE_CONVERSION+8192), can);
 }
 
 // moves servo to a new angle using raw register values (0 to 360 is 0 - 16383)
-void REG_POSITION_NEW_RAW(uint8_t servoId, uint16_t regVal, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can) {
-      genericWriteMessage(0x1E, servoId, regVal, can);
+void REG_POSITION_NEW_RAW(uint32_t canId, bool extended,  uint8_t servoId, uint16_t regVal, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can) {
+      genericWriteMessage(canId, extended,  0x1E, servoId, regVal, can);
 }
 
 // +1 rotates servo 360 in the + direction
-void REG_TURN_NEW(uint8_t servoId, int16_t rotations, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can) {
-      genericWriteMessage(0x24, servoId, rotations, can);
+void REG_TURN_NEW(uint32_t canId, bool extended,  uint8_t servoId, int16_t rotations, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can) {
+      genericWriteMessage(canId, extended,  0x24, servoId, rotations, can);
 } // to enable you must change run mode to continuous, save the config, then restart the servo
 
 // 0: multi-turn mode, 1: servo mode
-void REG_RUN_MODE(uint8_t servoId, uint16_t mode, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can) {
-      genericWriteMessage(0x44, servoId, mode, can);
+void REG_RUN_MODE(uint32_t canId, bool extended,  uint8_t servoId, uint16_t mode, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can) {
+      genericWriteMessage(canId, extended,  0x44, servoId, mode, can);
 }
 
 // saves current reg values for reset
-void REG_CONFIG_SAVE(uint8_t servoId, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can) {
-      genericWriteMessage(0x70, servoId, 0xFFFF, can);
+void REG_CONFIG_SAVE(uint32_t canId, bool extended,  uint8_t servoId, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can) {
+      genericWriteMessage(canId, extended,  0x70, servoId, 0xFFFF, can);
 }
 
 // resets servo, applies config changes
-void REG_POWER_CONFIG(uint8_t servoId, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can) {
-      genericWriteMessage(0x46, servoId, 0x0001, can);
+void REG_POWER_CONFIG(uint32_t canId, bool extended,  uint8_t servoId, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can) {
+      genericWriteMessage(canId, extended,  0x46, servoId, 0x0001, can);
 }
 
 // sets position max limit(s)
-void REG_POSITION_MAX_LIMIT(uint8_t servoId, double maxAngle, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can) {
-      genericWriteMessage(0xB0, servoId, maxAngle, can);
+void REG_POSITION_MAX_LIMIT(uint32_t canId, bool extended,  uint8_t servoId, double maxAngle, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can) {
+      genericWriteMessage(canId, extended,  0xB0, servoId, maxAngle, can);
 }
 
 // sets position min limit(s)
-void REG_POSITION_MIN_LIMIT(uint8_t servoId, double minAngle, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can) {
-      genericWriteMessage(0xB2, servoId, minAngle, can);
+void REG_POSITION_MIN_LIMIT(uint32_t canId, bool extended,  uint8_t servoId, double minAngle, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can) {
+      genericWriteMessage(canId, extended,  0xB2, servoId, minAngle, can);
 }
 
 // restore factory default
-void REG_FACTORY_DEFAULT(uint8_t servoId, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can) {
-      genericWriteMessage(0x6E, servoId, 0x0F0F, can);
+void REG_FACTORY_DEFAULT(uint32_t canId, bool extended,  uint8_t servoId, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can) {
+      genericWriteMessage(canId, extended,  0x6E, servoId, 0x0F0F, can);
 }
 
 // change velocity max speed
-void REG_VELOCITY_MAX(uint8_t servoId, int RPM, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can) {
+void REG_VELOCITY_MAX(uint32_t canId, bool extended,  uint8_t servoId, int RPM, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can) {
     double speed = RPM * 27.33;
-    genericWriteMessage(0x54, servoId, speed, can);
+    genericWriteMessage(canId, extended,  0x54, servoId, speed, can);
 }
 
 // query for servo turn count
-void REG_TURN_COUNT(uint8_t servoId, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can) {
-      genericReadMessage(0x18, servoId, can);
+void REG_TURN_COUNT(uint32_t canId, uint8_t servoId, bool extended,  FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can) {
+      genericReadMessage(canId, extended, 0x18, servoId, can);
 }
 
-// decodes reported turn count
+// decodes reported turn count extended,
 int16_t decodeTurnCount(CAN_message_t msg) {
       return (int16_t)genericDecoder(msg);
 }
 
 // query for low value of current position (0 - 65535, 4096 = 90 deg)
-void REG_32BITS_POSITION_L(uint8_t servoId, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can) {
-      genericReadMessage(0x1A, servoId, can);
+void REG_32BITS_POSITION_L(uint32_t canId, uint8_t servoId, bool extended,  FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can) {
+      genericReadMessage(canId, extended, 0x1A, servoId, can);
 }  
 
 // decodes CAN position response integer value between -180 and 180 
@@ -117,8 +119,8 @@ double decodePositionLo(CAN_message_t msg) {
 }
 
 // query for high value of position (65535 - 2^31, 4096 = 90 deg)
-void REG_32BITS_POSITION_H(uint8_t servoId, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can) {
-      genericReadMessage(0x1C, servoId, can);
+void REG_32BITS_POSITION_H(uint32_t canId, uint8_t servoId, bool extended,  FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can) {
+      genericReadMessage(canId, extended, 0x1C, servoId, can);
 } // TODO: write decoder
 
 // // this is BROKEN and I DON'T KNOW WHY (problem exists on hitec prog too)
@@ -127,8 +129,8 @@ void REG_32BITS_POSITION_H(uint8_t servoId, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZ
 // }
 
 // query for current servo velocity
-void REG_VOLTAGE(uint8_t servoId, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can) {
-      genericReadMessage(0x12, servoId, can);
+void REG_VOLTAGE(uint32_t canId, uint8_t servoId, bool extended,  FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can) {
+      genericReadMessage(canId, extended, 0x12, servoId, can);
 }
 
 // decodes CAN voltage response
@@ -137,8 +139,8 @@ double decodeVoltage(CAN_message_t msg) {
 }
 
 // query mcu temperature
-void REG_MCU_TEMPER(uint8_t servoId, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can) {
-      genericReadMessage(0x14, servoId, can);
+void REG_MCU_TEMPER(uint32_t canId, uint8_t servoId, bool extended,  FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can) {
+      genericReadMessage(canId, extended, 0x14, servoId, can);
 }
 
 // decode CAN temperature response 
@@ -147,6 +149,32 @@ uint16_t decodeTemp(CAN_message_t msg) {
 }
 
 // assigns a servo a new ID. not sure how this works for multiple servos on one can line
-void REG_ID(uint8_t servoId, uint16_t newId, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can) {
-      genericWriteMessage(0x32, servoId, newId, can);
+void REG_ID(uint32_t canId, bool extended, uint8_t servoId, uint16_t newId, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can) {
+      genericWriteMessage(canId, extended, 0x32, servoId, newId, can);
 }   // requires config save and reset
+
+// sets CAN mode (0 for 2.0, 1 for 2.0B)
+void REG_CAN_MODE(uint32_t canId, uint16_t servoId, bool mode, bool extended, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can) {
+      genericWriteMessage(canId, extended, 0x6A, servoId, mode, can);
+}
+
+// sets high word for CAN bus ID (requires power cycle)
+void REG_CAN_BUS_ID_H(uint32_t currCanId, uint16_t newCanIdH, uint16_t servoId, bool extended, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can) {
+      if (newCanIdH > 8191) {
+            Serial.println("newCanIdH exceeds acceptable value");
+            return;
+      }
+      
+      genericWriteMessage(currCanId, extended, 0x3C, servoId, newCanIdH, can);
+}
+
+// sets low word for CAN bus ID (requires power cycle)
+void REG_CAN_BUS_ID_L(uint32_t currCanId, uint16_t newCanIdL, uint16_t servoId, bool extended, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can) {
+      if (newCanIdL > 65535) {
+            Serial.println("newCanIdH exceeds acceptable value");
+            return;
+      }
+
+      genericWriteMessage(currCanId, extended, 0x3E, servoId, newCanIdL, can);
+}
+
--- servocan.hh
+++ servocan.hh
@@ -15,29 +15,31 @@
 
 #ifndef SERVOCAN_H
 #define SERVOCAN_H
-void printRaw(CAN_message_t msg);
-void REG_POSITION_NEW(uint8_t servoId, double usrAngle, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can);
-void REG_POSITION_NEW_RAW(uint8_t servoId, uint16_t regVal, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can);
-void REG_TURN_NEW(uint8_t servoId, int16_t rotations, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can);
-void REG_RUN_MODE(uint8_t servoId, uint16_t mode, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can);
-void REG_CONFIG_SAVE(uint8_t servoId, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can);
-void REG_POWER_CONFIG(uint8_t servoId, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can);
-void REG_POSITION_MAX_LIMIT(uint8_t servoId, double maxAngle, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can);
-void REG_POSITION_MIN_LIMIT(uint8_t servoId, double minAngle, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can);
-void REG_FACTORY_DEFAULT(uint8_t servoId, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can);
-void REG_VELOCITY_MAX(uint8_t servoId, int RPM, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can);
-void REG_TURN_COUNT(uint8_t servoId, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can);
-int16_t decodeTurnCount(CAN_message_t msg);
-void REG_32BITS_POSITION_L(uint8_t servoId, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can);
-double decodePositionLo(CAN_message_t msg);
-void REG_32BITS_POSITION_H(uint8_t servoId, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can);
-// void REG_POSITION(uint8_t servoId, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can);         // broken
-void REG_VOLTAGE(uint8_t servoId, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can);
-double decodeVoltage(CAN_message_t msg);
-void REG_MCU_TEMPER(uint8_t servoId, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can);
-uint16_t decodeTemp(CAN_message_t msg);
-void REG_ID(uint8_t servoId, uint16_t newId, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can);
 
+void printRaw(CAN_message_t servo_message);
+void REG_POSITION_NEW(uint32_t canId, bool extended, uint8_t servoId, double usrAngle, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can);
+void REG_POSITION_NEW_RAW(uint32_t canId, bool extended,  uint8_t servoId, uint16_t regVal, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can);
+void REG_TURN_NEW(uint32_t canId, bool extended,  uint8_t servoId, int16_t rotations, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can);
+void REG_RUN_MODE(uint32_t canId, bool extended,  uint8_t servoId, uint16_t mode, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can);
+void REG_CONFIG_SAVE(uint32_t canId, bool extended,  uint8_t servoId, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can);
+void REG_POWER_CONFIG(uint32_t canId, bool extended,  uint8_t servoId, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can);
+void REG_POSITION_MAX_LIMIT(uint32_t canId, bool extended,  uint8_t servoId, double maxAngle, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can);
+void REG_POSITION_MIN_LIMIT(uint32_t canId, bool extended,  uint8_t servoId, double minAngle, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can);
+void REG_FACTORY_DEFAULT(uint32_t canId, bool extended,  uint8_t servoId, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can);
+void REG_VELOCITY_MAX(uint32_t canId, bool extended,  uint8_t servoId, int RPM, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can);
+void REG_TURN_COUNT(uint32_t canId, uint8_t servoId, bool extended,  FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can);
+int16_t decodeTurnCount(CAN_message_t msg);
+void REG_32BITS_POSITION_L(uint32_t canId, uint8_t servoId, bool extended,  FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can);
+double decodePositionLo(CAN_message_t msg);
+void REG_32BITS_POSITION_H(uint32_t canId, uint8_t servoId, bool extended,  FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can);
+void REG_VOLTAGE(uint32_t canId, uint8_t servoId, bool extended,  FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can);
+double decodeVoltage(CAN_message_t msg);
+void REG_MCU_TEMPER(uint32_t canId, uint8_t servoId, bool extended,  FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can);
+uint16_t decodeTemp(CAN_message_t msg);
+void REG_ID(uint32_t canId, bool extended, uint8_t servoId, uint16_t newId, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can);
+void REG_CAN_MODE(uint32_t canId, uint16_t servoId, bool mode, bool extended, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can);
+void REG_CAN_BUS_ID_H(uint32_t currCanId, uint16_t newCanIdH, uint16_t servoId, bool extended, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can);
+void REG_CAN_BUS_ID_L(uint32_t currCanId, uint16_t newCanIdL, uint16_t servoId, bool extended, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can);
 extern CAN_message_t servo_message;
 
 #endif
