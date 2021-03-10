0a84ccab97a5b1242401271c231ff14bd5b208a6
ian ulanday <ulanday.ian@gmail.com>
2021-03-10 15:17:45 -0700

    changed order of function reg_turn_count args

--- servocan.hh
+++ servocan.hh
@@ -28,7 +28,7 @@ void REG_POSITION_MIN_LIMIT(uint32_t canId, bool extended,  uint8_t servoId, dou
 void REG_FACTORY_DEFAULT(uint32_t canId, bool extended,  uint8_t servoId, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can);
 void REG_VELOCITY_MAX(uint32_t canId, bool extended,  uint8_t servoId, int RPM, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can);
 void REG_VELOCITY(uint32_t canId, bool extended, uint8_t servoId, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can);
-void REG_TURN_COUNT(uint32_t canId, uint8_t servoId, bool extended,  FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can);
+void REG_TURN_COUNT(uint32_t canId, bool extended, uint8_t servoId, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can);
 int16_t decodeTurnCount(CAN_message_t msg);
 void REG_32BITS_POSITION_L(uint32_t canId, bool extended, uint8_t servoId, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can);
 double decodePositionLo(CAN_message_t msg);
