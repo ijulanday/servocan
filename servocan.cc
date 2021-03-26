d2caff82da54ed0ee1a2787904f1178b33bed688
Ian Ulanday <ulanday.ian@gmail.com>
2021-03-26 14:54:55 -0700

    fixing things

--- servocan.cc
+++ servocan.cc
@@ -48,64 +48,64 @@ uint16_t genericDecoder(CAN_message_t msg) {
 }
 
 // moves servo to an angle from -180 to 180 degrees
-void REG_POSITION_NEW(uint32_t canId, bool extended, uint8_t servoId, double usrAngle) {
-      genericWriteMessage(canId, extended, 0x1E, servoId, uint16_t(usrAngle*ANGLE_CONVERSION+8192));
+CAN_message_t REG_POSITION_NEW(uint32_t canId, bool extended, uint8_t servoId, double usrAngle) {
+      return genericWriteMessage(canId, extended, 0x1E, servoId, uint16_t(usrAngle*ANGLE_CONVERSION+8192));
 }
 
 // moves servo to a new angle using raw register values (0 to 360 is 0 - 16383)
-void REG_POSITION_NEW_RAW(uint32_t canId, bool extended,  uint8_t servoId, uint16_t regVal) {
-      genericWriteMessage(canId, extended,  0x1E, servoId, regVal);
+CAN_message_t REG_POSITION_NEW_RAW(uint32_t canId, bool extended,  uint8_t servoId, uint16_t regVal) {
+      return genericWriteMessage(canId, extended,  0x1E, servoId, regVal);
 }
 
 // +1 rotates servo 360 in the + direction
-void REG_TURN_NEW(uint32_t canId, bool extended,  uint8_t servoId, int16_t rotations) {
-      genericWriteMessage(canId, extended,  0x24, servoId, rotations);
+CAN_message_t REG_TURN_NEW(uint32_t canId, bool extended,  uint8_t servoId, int16_t rotations) {
+      return genericWriteMessage(canId, extended,  0x24, servoId, rotations);
 } // to enable you must change run mode to continuous, save the config, then restart the servo
 
 // 0: multi-turn mode, 1: servo mode
-void REG_RUN_MODE(uint32_t canId, bool extended,  uint8_t servoId, uint16_t mode) {
-      genericWriteMessage(canId, extended,  0x44, servoId, mode);
+CAN_message_t REG_RUN_MODE(uint32_t canId, bool extended,  uint8_t servoId, uint16_t mode) {
+      return genericWriteMessage(canId, extended,  0x44, servoId, mode);
 }
 
 // saves current reg values for reset
-void REG_CONFIG_SAVE(uint32_t canId, bool extended,  uint8_t servoId) {
-      genericWriteMessage(canId, extended,  0x70, servoId, 0xFFFF);
+CAN_message_t REG_CONFIG_SAVE(uint32_t canId, bool extended,  uint8_t servoId) {
+      return genericWriteMessage(canId, extended,  0x70, servoId, 0xFFFF);
 }
 
 // resets servo, applies config changes
-void REG_POWER_CONFIG(uint32_t canId, bool extended,  uint8_t servoId) {
-      genericWriteMessage(canId, extended,  0x46, servoId, 0x0001);
+CAN_message_t REG_POWER_CONFIG(uint32_t canId, bool extended,  uint8_t servoId) {
+      return genericWriteMessage(canId, extended,  0x46, servoId, 0x0001);
 }
 
 // sets position max limit(s)
-void REG_POSITION_MAX_LIMIT(uint32_t canId, bool extended,  uint8_t servoId, double maxAngle) {
-      genericWriteMessage(canId, extended,  0xB0, servoId, maxAngle);
+CAN_message_t REG_POSITION_MAX_LIMIT(uint32_t canId, bool extended,  uint8_t servoId, double maxAngle) {
+      return genericWriteMessage(canId, extended,  0xB0, servoId, maxAngle);
 }
 
 // sets position min limit(s)
-void REG_POSITION_MIN_LIMIT(uint32_t canId, bool extended,  uint8_t servoId, double minAngle) {
-      genericWriteMessage(canId, extended,  0xB2, servoId, minAngle);
+CAN_message_t REG_POSITION_MIN_LIMIT(uint32_t canId, bool extended,  uint8_t servoId, double minAngle) {
+      return genericWriteMessage(canId, extended,  0xB2, servoId, minAngle);
 }
 
 // restore factory default
-void REG_FACTORY_DEFAULT(uint32_t canId, bool extended,  uint8_t servoId) {
-      genericWriteMessage(canId, extended,  0x6E, servoId, 0x0F0F);
+CAN_message_t REG_FACTORY_DEFAULT(uint32_t canId, bool extended,  uint8_t servoId) {
+      return genericWriteMessage(canId, extended,  0x6E, servoId, 0x0F0F);
 }
 
 // change velocity max speed
-void REG_VELOCITY_MAX(uint32_t canId, bool extended,  uint8_t servoId, int RPM) {
+CAN_message_t REG_VELOCITY_MAX(uint32_t canId, bool extended,  uint8_t servoId, int RPM) {
     double speed = RPM * 27.33;
-    genericWriteMessage(canId, extended,  0x54, servoId, speed);
+    return genericWriteMessage(canId, extended,  0x54, servoId, speed);
 }
 
 // query for current servo velocity
-void REG_VELOCITY(uint32_t canId, bool extended, uint8_t servoId) {
-      genericReadMessage(canId, extended, 0x0E, servoId);
+CAN_message_t REG_VELOCITY(uint32_t canId, bool extended, uint8_t servoId) {
+     return genericReadMessage(canId, extended, 0x0E, servoId);
 }
 
 // query for servo turn count
-void REG_TURN_COUNT(uint32_t canId, bool extended, uint8_t servoId) {
-      genericReadMessage(canId, extended, 0x18, servoId);
+CAN_message_t REG_TURN_COUNT(uint32_t canId, bool extended, uint8_t servoId) {
+    return  genericReadMessage(canId, extended, 0x18, servoId);
 }
 
 // decodes reported turn count extended,
@@ -114,7 +114,7 @@ int16_t decodeTurnCount(CAN_message_t msg) {
 }
 
 // query for low value of current position (0 - 65535, 4096 = 90 deg)
-void REG_32BITS_POSITION_L(uint32_t canId, bool extended, uint8_t servoId) {
+CAN_message_t REG_32BITS_POSITION_L(uint32_t canId, bool extended, uint8_t servoId) {
       genericReadMessage(canId, extended, 0x1A, servoId);
 }  
 
@@ -125,18 +125,18 @@ double decodePositionLo(CAN_message_t msg) {
 }
 
 // query for high value of position (65535 - 2^31, 4096 = 90 deg)
-void REG_32BITS_POSITION_H(uint32_t canId, uint8_t servoId, bool extended) {
-      genericReadMessage(canId, extended, 0x1C, servoId);
+CAN_message_t REG_32BITS_POSITION_H(uint32_t canId, uint8_t servoId, bool extended) {
+     return genericReadMessage(canId, extended, 0x1C, servoId);
 } // TODO: write decoder
 
 // // this is BROKEN and I DON'T KNOW WHY (problem exists on hitec prog too)
-// void REG_POSITION(uint8_t servoId) {
+// CAN_message_t REG_POSITION(uint8_t servoId) {
 //       genericReadMessage(0x0C, servoId);
 // }
 
 // query for current servo velocity
-void REG_VOLTAGE(uint32_t canId, uint8_t servoId, bool extended) {
-      genericReadMessage(canId, extended, 0x12, servoId);
+CAN_message_t REG_VOLTAGE(uint32_t canId, uint8_t servoId, bool extended) {
+      return genericReadMessage(canId, extended, 0x12, servoId);
 }
 
 // decodes CAN voltage response
@@ -145,8 +145,8 @@ double decodeVoltage(CAN_message_t msg) {
 }
 
 // query mcu temperature
-void REG_MCU_TEMPER(uint32_t canId, uint8_t servoId, bool extended) {
-      genericReadMessage(canId, extended, 0x14, servoId);
+CAN_message_t REG_MCU_TEMPER(uint32_t canId, uint8_t servoId, bool extended) {
+      return genericReadMessage(canId, extended, 0x14, servoId);
 }
 
 // decode CAN temperature response 
@@ -155,93 +155,93 @@ uint16_t decodeTemp(CAN_message_t msg) {
 }
 
 // assigns a servo a new ID. not sure how this works for multiple servos on one can line
-void REG_ID(uint32_t canId, bool extended, uint8_t servoId, uint16_t newId) {
-      genericWriteMessage(canId, extended, 0x32, servoId, newId);
+CAN_message_t REG_ID(uint32_t canId, bool extended, uint8_t servoId, uint16_t newId) {
+      return genericWriteMessage(canId, extended, 0x32, servoId, newId);
 }   // requires config save and reset
 
 // sets CAN mode (0 for 2.0, 1 for 2.0B)
-void REG_CAN_MODE(uint32_t canId, bool extended, uint16_t servoId, bool mode) {
-      genericWriteMessage(canId, extended, 0x6A, servoId, mode);
+CAN_message_t REG_CAN_MODE(uint32_t canId, bool extended, uint16_t servoId, bool mode) {
+      return genericWriteMessage(canId, extended, 0x6A, servoId, mode);
 }
 
 // sets high word for CAN bus ID (requires power cycle)
-void REG_CAN_BUS_ID_H(uint32_t currCanId, bool extended, uint16_t newCanIdH, uint16_t servoId) {
+CAN_message_t REG_CAN_BUS_ID_H(uint32_t currCanId, bool extended, uint16_t newCanIdH, uint16_t servoId) {
       if (newCanIdH > 8191) {
             Serial.println("newCanIdH exceeds acceptable value");
             return;
       }
       
-      genericWriteMessage(currCanId, extended, 0x3C, servoId, newCanIdH);
+      return genericWriteMessage(currCanId, extended, 0x3C, servoId, newCanIdH);
 }
 
 // sets low word for CAN bus ID (requires power cycle)
-void REG_CAN_BUS_ID_L(uint32_t currCanId, bool extended, uint16_t newCanIdL, uint16_t servoId) {
+CAN_message_t REG_CAN_BUS_ID_L(uint32_t currCanId, bool extended, uint16_t newCanIdL, uint16_t servoId) {
       if (newCanIdL > 65535) {
             Serial.println("newCanIdH exceeds acceptable value");
             return;
       }
 
-      genericWriteMessage(currCanId, extended, 0x3E, servoId, newCanIdL);
+      return genericWriteMessage(currCanId, extended, 0x3E, servoId, newCanIdL);
 }
 
 // write to user memory 1
-void write_REG_USER_1(uint32_t canId, bool extended, uint16_t servoId, uint16_t data) {
-      genericWriteMessage(canId, extended, 0xCC, servoId, data);
+CAN_message_t write_REG_USER_1(uint32_t canId, bool extended, uint16_t servoId, uint16_t data) {
+      return genericWriteMessage(canId, extended, 0xCC, servoId, data);
 }
 
 // write to user memory 2
-void write_REG_USER_2(uint32_t canId, bool extended, uint16_t servoId, uint16_t data) {
-      genericWriteMessage(canId, extended, 0xCE, servoId, data);
+CAN_message_t write_REG_USER_2(uint32_t canId, bool extended, uint16_t servoId, uint16_t data) {
+      return genericWriteMessage(canId, extended, 0xCE, servoId, data);
 }
 
 // write to reg_echo (volitile memory)
-void write_REG_ECHO(uint32_t canId, bool extended, uint16_t servoId, uint16_t data) {
-      genericWriteMessage(canId, extended, 0xC6, servoId, data);
+CAN_message_t write_REG_ECHO(uint32_t canId, bool extended, uint16_t servoId, uint16_t data) {
+      return genericWriteMessage(canId, extended, 0xC6, servoId, data);
 }
 // read user memory 1
-void read_REG_USER_1(uint32_t canId, bool extended, uint16_t servoId) {
-      genericReadMessage(canId, extended, 0xCC, servoId);
+CAN_message_t read_REG_USER_1(uint32_t canId, bool extended, uint16_t servoId) {
+      return genericReadMessage(canId, extended, 0xCC, servoId);
 }
 
 // read user memory 2
-void read_REG_USER_2(uint32_t canId, bool extended, uint16_t servoId) {
-      genericReadMessage(canId, extended, 0xCE, servoId);
+CAN_message_t read_REG_USER_2(uint32_t canId, bool extended, uint16_t servoId) {
+      return genericReadMessage(canId, extended, 0xCE, servoId);
 }
 
 // read reg echo
-void read_REG_ECHO(uint32_t canId, bool extended, uint16_t servoId) {
-      genericReadMessage(canId, extended, 0xC6, servoId);
+CAN_message_t read_REG_ECHO(uint32_t canId, bool extended, uint16_t servoId) {
+      return genericReadMessage(canId, extended, 0xC6, servoId);
 }
 
 // sets stream time
-void REG_STREAM_TIME(uint32_t canId, bool extended, uint16_t servoId, uint16_t periodMs) {
-      genericWriteMessage(canId, extended, 0x2E, servoId, periodMs);
+CAN_message_t REG_STREAM_TIME(uint32_t canId, bool extended, uint16_t servoId, uint16_t periodMs) {
+      return genericWriteMessage(canId, extended, 0x2E, servoId, periodMs);
 }
 
 // sets stream mode (0 off, 1 on)
-void REG_STREAM_MODE(uint32_t canId, bool extended, uint16_t servoId, bool mode) {
-      genericWriteMessage(canId, extended, 0x30, servoId, mode);
+CAN_message_t REG_STREAM_MODE(uint32_t canId, bool extended, uint16_t servoId, bool mode) {
+      return genericWriteMessage(canId, extended, 0x30, servoId, mode);
 }
 
 /** these don't work don't mess with 'em for now
 // sets custom stream address 0
-void REG_STREAM_ADDR_0(uint32_t canId, bool extended, uint16_t servoId, uint8_t addr0, uint8_t addr1) {
-      genericWriteMessage(canId, extended, 0xE2, servoId, ((uint16_t)(addr0) << 8) | (uint16_t)addr1);
+CAN_message_t REG_STREAM_ADDR_0(uint32_t canId, bool extended, uint16_t servoId, uint8_t addr0, uint8_t addr1) {
+      return genericWriteMessage(canId, extended, 0xE2, servoId, ((uint16_t)(addr0) << 8) | (uint16_t)addr1);
 }
 
 // sets custom stream address 1
-void REG_STREAM_ADDR_1(uint32_t canId, bool extended, uint16_t servoId, uint8_t addr0, uint8_t addr1) {
-      genericWriteMessage(canId, extended, 0xE4, servoId, ((uint16_t)(addr0) << 8) | (uint16_t)addr1);
+CAN_message_t REG_STREAM_ADDR_1(uint32_t canId, bool extended, uint16_t servoId, uint8_t addr0, uint8_t addr1) {
+      return genericWriteMessage(canId, extended, 0xE4, servoId, ((uint16_t)(addr0) << 8) | (uint16_t)addr1);
 }
 
 // sets custom stream address 2
-void REG_STREAM_ADDR_2(uint32_t canId, bool extended, uint16_t servoId, uint8_t addr0, uint8_t addr1) {
-      genericWriteMessage(canId, extended, 0xE6, servoId, ((uint16_t)(addr0) << 8) | (uint16_t)addr1);
+CAN_message_t REG_STREAM_ADDR_2(uint32_t canId, bool extended, uint16_t servoId, uint8_t addr0, uint8_t addr1) {
+      return genericWriteMessage(canId, extended, 0xE6, servoId, ((uint16_t)(addr0) << 8) | (uint16_t)addr1);
 }
 
 // sets custom stream address 3
-void REG_STREAM_ADDR_3(uint32_t canId, bool extended, uint16_t servoId, uint8_t addr0, uint8_t addr1) {
-      genericWriteMessage(canId, extended, 0xE8, servoId, ((uint16_t)(addr0) << 8) | (uint16_t)addr1);
+CAN_message_t REG_STREAM_ADDR_3(uint32_t canId, bool extended, uint16_t servoId, uint8_t addr0, uint8_t addr1) {
+      return genericWriteMessage(canId, extended, 0xE8, servoId, ((uint16_t)(addr0) << 8) | (uint16_t)addr1);
 }
 */
 
